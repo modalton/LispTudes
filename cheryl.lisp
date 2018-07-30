38282fa3953e26a4db83b5557afd640eb1500d31
Michael Dalton <modintexas@gmail.com>
2018-07-29 17:48:36 -0700

    working version with solution

--- cheryl.lisp
+++ cheryl.lisp
@@ -4,7 +4,7 @@
 
 (defun month (date) (car date))
 
-(defun day (date) (cdr date))
+(defun day (date) (cadr date))
 
 (defun tell (part &optional(possible_dates dates))
   (loop for (month day) in possible_dates
@@ -22,9 +22,16 @@
     (and (not (know possible_dates))
          (= (length possible_dates) (length (remove-if #'(lambda (x)(<= (length(tell (cadr x))) 1)) possible_dates )))  )))
 
-(defun statement4 (date) (date))
+(defun statement4 (date)
+  "Bernard: At first I don't know when Cheryl's birthday is, but I know now."
+  (let ((at_first (tell (day date))))
+    (and (not (know at_first))
+         (know (remove-if-not #'statement3 at_first)))
+    ))
 
-(defun statement5 (date) (date))
+(defun statement5 (date)
+  "Albert: Then I also know when Cheryl's birthday is."
+  (know (remove-if-not #'statement4 (tell (month date)))))
 
 (defun statements3to5 (date) (and (statement3 date) (statement4 date) (statement5 date)))
 
